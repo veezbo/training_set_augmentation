15cbc62fa7ba98e4dede39030cc093bcd65200a6
Vibhor Kumar <veezbo@kuma.local>
2016-01-16 20:49:23 -0500

    Fixed indexing in patchsizing

--- subvolumes_and_interpolatednoise.py
+++ subvolumes_and_interpolatednoise.py
@@ -253,7 +253,7 @@ def getSampleVolumes(image_stack, target_stack, input_padding, data_patchsize, l
         # Loop through until we find offsets that work
         while True:
 
-            data_offset = [randint(0, data_stack - data_patchsize[0]), randint(0, data_cols - data_patchsize[1] - 1), randint(0, data_rows - data_patchsize[2] - 1)]
+            data_offset = [randint(0, data_stack - data_patchsize[1]), randint(0, data_cols - data_patchsize[2] - 1), randint(0, data_rows - data_patchsize[3] - 1)]
             label_offset = [data_offset[di] + int(ceil(input_padding[di] / float(2))) for di in range(0, len(input_padding))]
 
             print "Data offset and patchsize: ", data_offset, data_patchsize
@@ -263,8 +263,8 @@ def getSampleVolumes(image_stack, target_stack, input_padding, data_patchsize, l
             if arePointsInside(data_dim, getFourCorners(data_offset[1:], data_patchsize[2:]), angle) and arePointsInside(label_dim, getFourCorners(label_offset[1:], label_patchsize[2:]), angle):
             # if arePointsInside(data_dim, getFourCorners(data_offset[1:], data_patchsize[2:]), angle):
                 # Get patches of volume based on offsets and sizes of volumes
-                data_patch = rot_ims[:, data_offset[0]:data_offset[0]+data_patchsize[0], data_offset[1]:data_offset[1]+data_patchsize[1], data_offset[2]:data_offset[2]+data_patchsize[2]]
-                label_patch = rot_targs[:, label_offset[0]:label_offset[0]+label_patchsize[0], label_offset[1]:label_offset[1]+label_patchsize[1], label_offset[2]:label_offset[2]+label_patchsize[2]]
+                data_patch = rot_ims[:, data_offset[0]:data_offset[0]+data_patchsize[1], data_offset[1]:data_offset[1]+data_patchsize[2], data_offset[2]:data_offset[2]+data_patchsize[3]]
+                label_patch = rot_targs[:, label_offset[0]:label_offset[0]+label_patchsize[1], label_offset[1]:label_offset[1]+label_patchsize[2], label_offset[2]:label_offset[2]+label_patchsize[3]]
 
                 data_samples[i] = data_patch
                 label_samples[i] = label_patch
